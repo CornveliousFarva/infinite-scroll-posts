d5817438b63002902b60c3bba88512ad67978a7b
Greg Schuman <gcschuman@hotmail.com>
2020-09-15 20:50:55 -0700

    Added the script to show the posts in the DOM.

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ async function getPosts(){
 
     return data;
 }
+
+//Show posts in the DOM
+async function showPosts(){
+    const posts = await getPosts();
+
+    posts.forEach(post => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = `
+            <div class="number">${post.id}</div>
+            <div class="post-info">
+                <h2 class="post-title>${post.title}</h2>
+                <p class="post-body">${post.body}</p>
+            </div>
+        `;
+
+        postsContainer.appendChild(postEl);
+    })
+}
